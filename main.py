fdb636247224617cb4d94b5e3a10b2e570d0e1f0
zoeymattison <122073280+zoeymattison@users.noreply.github.com>
2026-08-25 08:51:56 -0700

    Update

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+from sys import exit
+
+WINDOW_SIZE=(640,480)
+screen=pygame.display.set_mode(WINDOW_SIZE)
+
+while True:
+    for event in pygame.event.get():
+        if pygame.event() == pygame.QUIT
+            pygame.QUIT
+            exit()
+            
