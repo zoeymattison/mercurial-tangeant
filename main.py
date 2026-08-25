ec4f8dce4ae4f394a86fffe8b740bdd1ef6ee645
zoeymattison <122073280+zoeymattison@users.noreply.github.com>
2026-08-25 08:04:25 -0700

    Update

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+# Version 0.0.1
+
 import pygame
+
+# The safest way to close the code / window is actually to use the system
+# call, "exit". So we need to import it here. Now the code will end, rather
+# than hitting a conidition that can't run (no video update error message from
+# pygame.display.update()
+
+from sys import exit
+
 pygame.init()
 WINDOW_SIZE=(640,480)
 screen=pygame.display.set_mode(WINDOW_SIZE)
+pygame.display.set_caption("Mercurial Tangeant")
 
 # in order to run a game with pygame, we need the code to never end, and loop
 # back on itself until told to quit. For that reason, we use a while loop that
@@ -15,6 +26,7 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+            exit()
     # !!IMPORTANT!! you must write pygame.display.update() at the top
     # the update function constantly updates the game display (screen) with
     # whatever is drawn in here.
